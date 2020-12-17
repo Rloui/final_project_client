7704ebc9df9c2b3f0a496a1eb5178dc2207b04e9
Rloui <69759725+Rloui@users.noreply.github.com>
2020-12-17 16:13:43 -0500

    login form updates with state

--- trava/src/App.js
+++ trava/src/App.js
@@ -2,6 +2,22 @@ import React from 'react'
 import Login from './components/Login'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // username: '',
+      // password: '',
+      // email: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    console.log(e.target.value)
+    this.setState({username: e.target.username})
+  }
+
+
   render() {
     return (
     <div className='container'>
--- trava/src/components/Login.jsx
+++ trava/src/components/Login.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.id] : e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log('you prevented the default')
+    }
+
     render() {
         return (
             <div>
-                This is the login page
+                This is the login Page
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='username'>Username</label>
+                    <input type="text" placeholder='username' value={this.state.username} onChange={this.handleChange} id='username'/>
+                    <label htmlFor='password'>Password</label>
+                    <input type="text" placeholder='password' value={this.state.password} onChange={this.handleChange} id='password'/>
+                    <label htmlFor='email'>Email</label>
+                    <input type="text" placeholder='email' value={this.state.email} onChange={this.handleChange} id='email'/>
+                    
+                    <button type='submit'>Sign Up</button>
+                    <button type='submit'>Log In</button>
+                </form>
             </div>
         )
     }
